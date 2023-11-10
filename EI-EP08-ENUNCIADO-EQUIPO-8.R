b468e7aa7377e4c6976a662a995dd1318bac3e0e
valenpy22 <valentina.campos.o@usach.cl>
2023-11-09 23:02:25 -0300

    feat: hay un error que se produce al querer ejecutar el segundo enunciado

--- EI-EP08-ENUNCIADO-EQUIPO-8.R
+++ EI-EP08-ENUNCIADO-EQUIPO-8.R
@@ -4,13 +4,16 @@
 # en la población de Chile?
 
 library(ggpubr)
+library(boot) # Para bootstrapping
+library(dplyr) # Para manipulación de datos
+library(multcomp) # Para comparaciones post-hoc
 
 dir <- getwd()
 
 setwd(dir)
 
 # Se leen los datos
-datos <- read.csv2("C:/WorkSpace/EP-Grupo-8/EP08 Datos CASEN 2017.csv", fileEncoding = "Latin1")
+datos <- read.csv2("Desktop/EP-Grupo-8/EP08 Datos CASEN 2017.csv", fileEncoding = "Latin1")
 
 head(datos)
 # Se hace la selección de una muestra aleatoria
@@ -19,7 +22,7 @@ n <- sample(101:149, 1)
 muestra <- datos[sample(nrow(datos), n), ]
 
 # Se define el número de simulaciones
-num_simulaciones <- 2999
+num_simulaciones <- 99
 diferencias <- numeric(num_simulaciones)
 
 for(i in 1:num_simulaciones){
@@ -48,3 +51,47 @@ if(ic_inf > 0 | ic_sup < 0) {
 # ¿Difieren significativamente los promedios de de horas de trabajo entre las 
 # personas según su nivel de educación (Sin educación, M. Hum. completa y 
 # profesional completo)?
+
+# Establecer la semilla para la reproducibilidad
+set.seed(100) # Reemplaza una_nueva_semilla con un número entero de tu elección
+
+# Leer los datos desde el CSV
+datos2 <- read.csv2("Desktop/EP-Grupo-8/EP08 Datos CASEN 2017.csv", fileEncoding = "Latin1")
+
+# Asegurarse de que 'educ' y 'o10' están en el formato correcto
+datos2$educ <- as.factor(datos2$educ)
+datos2$o10 <- as.numeric(as.character(datos2$o10))
+
+# Seleccionar una muestra aleatoria de hogares
+n2 <- sample(201:299, 1) # Un número aleatorio entre 200 y 300
+muestra2 <- sample_n(datos2, n2)
+
+# Realizar bootstrapping
+# Definir la estadística de interés
+media_trabajo <- function(data, indices) {
+  d <- data[indices, ] # Permite el muestreo con reemplazo
+  medias <- tapply(d$o10, d$educ, mean, na.rm = TRUE)
+  return(medias)
+}
+
+# Aplicar el método de bootstrapping
+resultados_bootstrap <- boot(data = muestra2, statistic = media_trabajo, R = 99)
+print(resultados_bootstrap)
+
+Análisis post-hoc con bootstrapping
+# Utilizaremos la función `glht` de `multcomp` para múltiples comparaciones
+modelo <- lm(o10 ~ educ, data = muestra2)
+comparaciones <- glht(modelo, linfct = mcp(educ = "Tukey"))
+# summary(comparaciones)
+
+# Podemos también realizar bootstrapping para las comparaciones post-hoc
+comparacion_posthoc <- function(data, indices) {
+  d <- data[indices, ]
+  modelo <- lm(o10 ~ educ, data = d)
+  comp <- glht(modelo, linfct = mcp(educ = "Tukey"))
+  cld <- summary(comp)$test$coefficients
+  return(cld)
+}
+
+# Aplicar bootstrapping a las comparaciones post-hoc
+resultados_posthoc_bootstrap <- boot(data = muestra2, statistic = comparacion_posthoc, R = 99)
